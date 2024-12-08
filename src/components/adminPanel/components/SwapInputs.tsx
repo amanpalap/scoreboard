97157a3d9f1df2bf76dd1972f005a2376326cbde
amanpalap <pal90589500@gmail.com>
2024-12-08 15:52:08 +0530

    updated: players name

--- src/components/adminPanel/components/SwapInputs.tsx
+++ src/components/adminPanel/components/SwapInputs.tsx
@@ -10,7 +10,7 @@ interface AdminPanelProps {
 }
 
 const SwapInputs: React.FC<AdminPanelProps> = ({ formData, setFormData }) => {
-    const players = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 6', 'Player 7', 'Player 8', 'Player 9', 'Player 10', 'Player 11', 'Player 12'];
+    const players = ['John Smith', 'Steve Johnson', 'James Anderson', 'Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 6', 'Player 7', 'Player 8', 'Player 9', 'Player 10', 'Player 11', 'Player 12'];
 
     const [isSwitch, setIsSwitch] = useState(false)
 
